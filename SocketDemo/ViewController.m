5ef8cd2ba92b480fdc667ec6a6612d4a937b3b2f
Xihahou <zhangt@jymoit.com>
2017-07-06 14:53:11 +0800

    initial

--- SocketDemo/ViewController.m
+++ SocketDemo/ViewController.m
@@ -7,8 +7,17 @@
 //
 
 #import "ViewController.h"
+#import <sys/socket.h>
+#import <netinet/in.h>
+#import <arpa/inet.h>
 
 @interface ViewController ()
+
+@property (nonatomic, assign) int clientSocket;
+@property (weak, nonatomic) IBOutlet UILabel *recvCoLabel;
+@property (weak, nonatomic) IBOutlet UITextField *ipTextField;
+@property (weak, nonatomic) IBOutlet UITextField *portTF;
+@property (weak, nonatomic) IBOutlet UITextField *sendContTF;
 
 @end
 
@@ -19,11 +28,55 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)startConnect:(id)sender {
+    if ([self.ipTextField.text isEqualToString:@""]) {
+        return;
+    }
+    if ([self.portTF.text isEqualToString:@""]) {
+        return;
+    }
+    if ([self connection:self.ipTextField.text port:self.portTF.text.intValue]) {
+        self.recvCoLabel.text = @"连接成功";
+    }else{
+        self.recvCoLabel.text = @"连接失败";
+    }
+}
+- (IBAction)sendMsg:(id)sender {
+    if ([self.sendContTF.text isEqualToString:@""]) {
+        return;
+    }
+    self.recvCoLabel.text = [self sendAndRecv:self.sendContTF.text];
 }
 
+
+- (BOOL)connection:(NSString *)hostText port:(int)port{
+    self.clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (self.clientSocket > 0) {
+        NSLog(@"socket success create %d",self.clientSocket);
+    }else{
+        NSLog(@"socket create failed");
+    }
+    
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr(hostText.UTF8String);
+    serverAddress.sin_port = htons(port);
+    int result = connect(self.clientSocket, (const struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    return result == 0;
+}
+
+- (NSString *)sendAndRecv:(NSString *)msg{
+    size_t sendLen = send(self.clientSocket, msg.UTF8String, strlen(msg.UTF8String), 0);
+    NSLog(@"%ld",sendLen);
+    uint8_t buffer[1024];
+    ssize_t recvLen = recv(self.clientSocket, buffer, sizeof(buffer), 0);
+    NSData *data = [NSData dataWithBytes:buffer length:recvLen];
+    NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    return str;
+}
+
+- (void)disConnect{
+    close(self.clientSocket);
+}
 
 @end
